da1fc1aa65310d95edcb97676270687c55aef48a
NoFruatGames <gamercombat57@gmail.com>
2025-01-13 20:25:14 +0200

    added http controller

--- src/Server/Services/GamesService/Program.cs
+++ src/Server/Services/GamesService/Program.cs
@@ -3,120 +3,11 @@
 using GamesService;
 using GamesService.Interfaces;
 using GamesService.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
-using System.Text.Json;
-using System.Text.Json.Nodes;
+using SharedApiUtils.Abstractons.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
-var auth = builder.Configuration.GetRequiredSection("Authentication");
-var validIssuers = auth.GetSection("ValidIssuers").Get<IEnumerable<string>>() ?? throw new ArgumentException("Valid issuers not declared");
-string metadataAddress = auth.GetValue<string>("MetadataAddress") ?? throw new ArgumentException("Metadata address is null");
-bool requireHttpsMetadata = auth.GetValue<bool>("HttpsMetadata");
-string adminRoleClaim = auth.GetValue<string>("AdminRoleClaim") ?? throw new ArgumentException("Admin role claim is null");
-bool ignoreSslValidation = auth.GetValue<bool>("IgnoreSslCertificateValidation");
-builder.Services.AddAuthentication("PrivateClientScheme")
-    .AddJwtBearer("PrivateClientScheme", options =>
-    {
-        options.RequireHttpsMetadata = requireHttpsMetadata;
-        options.Audience = auth.GetValue<string>("PrivateAudience") ?? throw new ArgumentException("Private client audience is null");
-        options.MetadataAddress = metadataAddress;
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidIssuers = validIssuers,
-        };
-        if (ignoreSslValidation)
-        {
-            options.BackchannelHttpHandler = new HttpClientHandler()
-            {
-                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
-            };
-        }
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
-            {
-                var claimsIdentity = context.Principal?.Identity as ClaimsIdentity;
-                if (claimsIdentity is null) return Task.CompletedTask;
-                claimsIdentity.AddClaim(new Claim("AuthenticationScheme", "PrivateClientScheme"));
-                return Task.CompletedTask;
-            }
-        };
-
-    })
-    .AddJwtBearer("PublicClientScheme", options =>
-    {
-        options.RequireHttpsMetadata = requireHttpsMetadata;
-        options.Audience = auth.GetValue<string>("PublicAudience") ?? throw new ArgumentException("Public client audience is null");
-        options.MetadataAddress = metadataAddress;
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidIssuers = validIssuers,
-        };
-        if (ignoreSslValidation)
-        {
-            options.BackchannelHttpHandler = new HttpClientHandler()
-            {
-                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
-            };
-        }
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
-            {
-                var claimsIdentity = context.Principal?.Identity as ClaimsIdentity;
-                if (claimsIdentity is null) return Task.CompletedTask;
-                claimsIdentity.AddClaim(new Claim("AuthenticationScheme", "PublicClientScheme"));
-
-                var realmAccessClaim = claimsIdentity.FindFirst("realm_access");
-                if (realmAccessClaim is null) return Task.CompletedTask;
-
-                var realmAccess = JsonNode.Parse(realmAccessClaim.Value);
-                if (realmAccess is null) return Task.CompletedTask;
-
-                var roles = realmAccess["roles"]?.AsArray();
-                if (roles is null) return Task.CompletedTask;
-                foreach (var role in roles)
-                {
-                    claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.Deserialize<string>()));
-                }
-
-                return Task.CompletedTask;
-            }
-        };
-    });
-
-
-builder.Services.AddAuthorization(options =>
-{
-
-    options.AddPolicy("AdminOrPrivateClient", policy =>
-    {
-        policy.RequireAuthenticatedUser();
-        policy.AuthenticationSchemes.Add("PublicClientScheme");
-        policy.AuthenticationSchemes.Add("PrivateClientScheme");
-        policy.RequireAssertion((context) =>
-        {
-            var scheme = context.User.Claims.FirstOrDefault(c => c.Type == "AuthenticationScheme");
-            if (scheme is null)
-                return false;
-
-            if (scheme.Value == "PublicClientScheme" && context.User.IsInRole(adminRoleClaim))
-                return true;
-            else if (scheme.Value == "PrivateClientScheme")
-                return true;
-            else
-                return false;
-        });
-    });
-    options.AddPolicy("AllAuthenticatedUsers", policy =>
-    {
-        policy.RequireAuthenticatedUser();
-        policy.AuthenticationSchemes.Add("PublicClientScheme");
-        policy.AuthenticationSchemes.Add("PrivateClientScheme");
-    });
-});
+builder.Services.AddCustomAuthentication(builder.Configuration);
 builder.Services.AddGrpc();
 builder.Services.AddDbContext<GamesService.GamesServerDbContext>(options =>
 {
@@ -124,6 +15,7 @@ builder.Services.AddDbContext<GamesService.GamesServerDbContext>(options =>
 });
 builder.Services.AddScoped<IGamesRepository, SqlServerGamesRepository>();
 builder.Services.AddScoped<IGamesService, GamesService.Services.GamesService>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
@@ -136,6 +28,6 @@ using (var scope = app.Services.CreateScope())
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client.");
+app.MapControllers();
 app.MapGrpcService<GamesService.Services.GamesRpcService>();
 app.Run();
---
+++ src/Server/Services/GamesService/Services/GamesServiceController.cs
@@ -0,0 +1,58 @@
+﻿using GamesService.Interfaces;
+using GamesService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesService.Services;
+
+[Route("/rest/GamesService")]
+[ApiController]
+public class GamesServiceController : ControllerBase
+{
+    private readonly IGamesService gamesService;
+
+
+    public GamesServiceController(IGamesService gamesService)
+    {
+        this.gamesService = gamesService;
+    }
+    [HttpPost("AddGame")]
+    [Authorize(Policy = "AdminOrPrivateClient")]
+    public async Task<IActionResult> AddGame([FromBody] GameInfoModel gameInfo)
+    {
+
+        string? errorMessage = await gamesService.AddGame(gameInfo);
+        if (string.IsNullOrEmpty(errorMessage))
+            return Created();
+        else
+            return BadRequest(errorMessage);
+    }
+
+    [Authorize(Policy = "AdminOrPrivateClient")]
+    [HttpDelete("DeleteGame/{gameId}")]
+    public async Task<IActionResult> DeleteGame([FromRoute] string gameId)
+    {
+        string? errorMessage = await gamesService.DeleteGame(gameId);
+        if (string.IsNullOrEmpty(errorMessage))
+            return NoContent();
+        else
+            return BadRequest(errorMessage);
+    }
+    [Authorize(Policy = "AdminOrPrivateClient")]
+    [HttpPut("UpdateGame")]
+    public async Task<IActionResult> UpdateGame([FromBody] GameInfoModel gameInfo)
+    {
+        string? errorMessage = await gamesService.UpdateGame(gameInfo);
+        if (string.IsNullOrEmpty(errorMessage))
+            return NoContent();
+        else
+            return BadRequest(errorMessage);
+    }
+    [Authorize(Policy = "AllAuthenticatedUsers")]
+    [HttpGet("GetGamesList")]
+    public async Task<IActionResult> GetGamesList()
+    {
+        var gamesList = await gamesService.GetGamesList();
+        return Ok(gamesList);
+    }
+}
