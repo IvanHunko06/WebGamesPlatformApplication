58dd6bbb2ac2f73b85b45759c7cb8d3da0785c26
Ivan Hunko <ivan.hunko.dan@gmail.com>
2025-01-14 14:42:24 +0200

    fixed authentication

--- src/Server/SharedApiUtils.Abstractons/Authentication/AuthenticationConfig.cs
+++ src/Server/SharedApiUtils.Abstractons/Authentication/AuthenticationConfig.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace SharedApiUtils.Abstractons.Authentication;
 
@@ -14,77 +14,77 @@ public static class AuthenticationConfig
     {
         services.Configure<AuthSettings>(configuration.GetRequiredSection("Authentication"));
         var serviceProvider = services.BuildServiceProvider();
-        var authSettings = serviceProvider.GetRequiredService<AuthSettings>();
+        var authSettings = serviceProvider.GetRequiredService<IOptions<AuthSettings>>().Value;
         services.AddAuthentication("PrivateClientScheme")
-    .AddJwtBearer("PrivateClientScheme", options =>
-    {
-        options.RequireHttpsMetadata = authSettings.HttpsMetadata;
-        options.Audience = authSettings.PrivateAudience;
-        options.MetadataAddress = authSettings.MetadataAddress;
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidIssuers = authSettings.ValidIssuers,
-        };
-        if (authSettings.IgnoreSslCertificateValidation)
-        {
-            options.BackchannelHttpHandler = new HttpClientHandler()
+            .AddJwtBearer("PrivateClientScheme", options =>
             {
-                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
-            };
-        }
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
-            {
-                var claimsIdentity = context.Principal?.Identity as ClaimsIdentity;
-                if (claimsIdentity is null) return Task.CompletedTask;
-                claimsIdentity.AddClaim(new Claim("AuthenticationScheme", "PrivateClientScheme"));
-                return Task.CompletedTask;
-            }
-        };
-
-    })
-    .AddJwtBearer("PublicClientScheme", options =>
-    {
-        options.RequireHttpsMetadata = authSettings.HttpsMetadata;
-        options.Audience = authSettings.PublicAudience;
-        options.MetadataAddress = authSettings.MetadataAddress;
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidIssuers = authSettings.ValidIssuers,
-        };
-        if (authSettings.IgnoreSslCertificateValidation)
-        {
-            options.BackchannelHttpHandler = new HttpClientHandler()
-            {
-                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
-            };
-        }
-        options.Events = new JwtBearerEvents
-        {
-            OnTokenValidated = context =>
-            {
-                var claimsIdentity = context.Principal?.Identity as ClaimsIdentity;
-                if (claimsIdentity is null) return Task.CompletedTask;
-                claimsIdentity.AddClaim(new Claim("AuthenticationScheme", "PublicClientScheme"));
-
-                var realmAccessClaim = claimsIdentity.FindFirst("realm_access");
-                if (realmAccessClaim is null) return Task.CompletedTask;
-
-                var realmAccess = JsonNode.Parse(realmAccessClaim.Value);
-                if (realmAccess is null) return Task.CompletedTask;
-
-                var roles = realmAccess["roles"]?.AsArray();
-                if (roles is null) return Task.CompletedTask;
-                foreach (var role in roles)
+                options.RequireHttpsMetadata = authSettings.HttpsMetadata;
+                options.Audience = authSettings.PrivateAudience;
+                options.MetadataAddress = authSettings.MetadataAddress;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
                 {
-                    claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.Deserialize<string>()));
+                    ValidIssuers = authSettings.ValidIssuers,
+                };
+                if (authSettings.IgnoreSslCertificateValidation)
+                {
+                    options.BackchannelHttpHandler = new HttpClientHandler()
+                    {
+                        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                    };
                 }
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var claimsIdentity = context.Principal?.Identity as ClaimsIdentity;
+                        if (claimsIdentity is null) return Task.CompletedTask;
+                        claimsIdentity.AddClaim(new Claim("AuthenticationScheme", "PrivateClientScheme"));
+                        return Task.CompletedTask;
+                    }
+                };
 
-                return Task.CompletedTask;
-            }
-        };
-    });
+            })
+            .AddJwtBearer("PublicClientScheme", options =>
+            {
+                options.RequireHttpsMetadata = authSettings.HttpsMetadata;
+                options.Audience = authSettings.PublicAudience;
+                options.MetadataAddress = authSettings.MetadataAddress;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidIssuers = authSettings.ValidIssuers,
+                };
+                if (authSettings.IgnoreSslCertificateValidation)
+                {
+                    options.BackchannelHttpHandler = new HttpClientHandler()
+                    {
+                        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                    };
+                }
+                options.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var claimsIdentity = context.Principal?.Identity as ClaimsIdentity;
+                        if (claimsIdentity is null) return Task.CompletedTask;
+                        claimsIdentity.AddClaim(new Claim("AuthenticationScheme", "PublicClientScheme"));
+
+                        var realmAccessClaim = claimsIdentity.FindFirst("realm_access");
+                        if (realmAccessClaim is null) return Task.CompletedTask;
+
+                        var realmAccess = JsonNode.Parse(realmAccessClaim.Value);
+                        if (realmAccess is null) return Task.CompletedTask;
+
+                        var roles = realmAccess["roles"]?.AsArray();
+                        if (roles is null) return Task.CompletedTask;
+                        foreach (var role in roles)
+                        {
+                            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.Deserialize<string>()));
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
         services.AddAuthorization(options =>
         {
 
