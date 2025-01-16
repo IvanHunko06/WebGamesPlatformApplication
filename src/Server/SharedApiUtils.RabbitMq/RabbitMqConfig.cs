340487c384d2ffc81a16cf3b7bf1ebd8617d4ea5
Ivan Hunko <ivan.hunko.dan@gmail.com>
2025-01-16 23:42:27 +0200

    updated RabbitMq utils

---
+++ src/Server/SharedApiUtils.RabbitMq/RabbitMqConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SharedApiUtils.RabbitMq;
+
+public static class RabbitMqConfig
+{
+    public static IServiceCollection AddCustomRabbitMq(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton(new RabbitMqConfiguration()
+        {
+            Host = configuration["RabbitMqConfiguration:Host"]!,
+            Username = configuration["RabbitMqConfiguration:Username"]!,
+            Password = configuration["RabbitMqConfiguration:Password"]!
+        });
+        services.AddSingleton<RabbitMqConnection>();
+        return services;
+    }
+}
