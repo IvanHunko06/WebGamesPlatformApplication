6b335363787c8d4cdc6cc0ece72c08c90c341473
Ivan Hunko <ivan.hunko.dan@gmail.com>
2025-01-16 23:40:12 +0200

    updated end session reasons

--- src/Server/SharedApiUtils.Abstractons/EndSessionReason.cs
+++ src/Server/SharedApiUtils.Abstractons/EndSessionReason.cs
@@ -5,4 +5,5 @@ public static class EndSessionReason
     public static string PlayerDisconnected { get; } = "PLAYER_DISCONNECTED";
     public static string DebugDelete { get; } = "DEBUG_DELETE";
     public static string RoomDeleted { get; } = "ROOM_DELETED";
+    public static string NormalFinish { get; } = "NORMAL_FINISH";
 }
