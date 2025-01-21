84dfce9af99c60268e35bc79a17b34b3322d0bce
Ivan Hunko <ivan.hunko.dan@gmail.com>
2025-01-21 18:34:45 +0200

    changed token accessor

--- src/Server/SharedApiUtils.Abstractons/AuthenticationTokenAccessor/AuthenticationTokenAccessor.cs
+++ src/Server/SharedApiUtils.Abstractons/AuthenticationTokenAccessor/AuthenticationTokenAccessor.cs
@@ -4,48 +4,80 @@ namespace SharedApiUtils.Abstractons.AuthenticationTokenAccessor;
 
 public class AuthenticationTokenAccessor
 {
-    private readonly bool ignoreSslVerification;
-    private readonly string authenticationUrl;
-    private readonly string clientSecret;
-    private readonly string clientId;
-    private readonly string tokenClaim;
-    public AuthenticationTokenAccessor(TokenAccessorConfiguration configuration)
+    private readonly PrivateTokenConfiguration? privateToken;
+    private readonly AdminTokenConfiguration? adminToken;
+
+    public AuthenticationTokenAccessor(PrivateTokenConfiguration? privateToken, AdminTokenConfiguration? adminToken)
     {
-        ignoreSslVerification = configuration.IgnoreSslVerification;
-        authenticationUrl = configuration.AuthenticationUrl;
-        clientSecret = configuration.ClientSecret;
-        clientId = configuration.ClientId;
-        tokenClaim = configuration.TokenClaim;
+        this.privateToken = privateToken;
+        this.adminToken = adminToken;
     }
-    public async Task<string> GetNewToken()
+    public async Task<string> GetPrivateClientToken()
     {
+        if (privateToken is null)
+            throw new ArgumentNullException("Private token configuration not found");
         try
         {
             HttpClientHandler httpClientHandler = new HttpClientHandler();
-            if (ignoreSslVerification)
+            if (privateToken.IgnoreSslVerification)
             {
                 httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
             }
             HttpClient httpClient = new HttpClient(httpClientHandler);
             var parameters = new Dictionary<string, string>
             {
-                {"client_id", clientId},
-                {"client_secret", clientSecret},
+                {"client_id", privateToken.ClientId},
+                {"client_secret", privateToken.ClientSecret},
                 {"grant_type", "client_credentials"},
                 {"scope","openid" }
             };
             var content = new FormUrlEncodedContent(parameters);
-            var response = await httpClient.PostAsync(authenticationUrl, content);
+            var response = await httpClient.PostAsync(privateToken.AuthenticationUrl, content);
             if (!response.IsSuccessStatusCode) return string.Empty;
 
             var responseString = await response.Content.ReadAsStringAsync();
             string? token = responseString;
-            if (tokenClaim is not null)
+            if (privateToken.TokenClaim is not null)
             {
                 var jsonObject = JsonSerializer.Deserialize<JsonObject>(responseString);
                 if (jsonObject is null) return string.Empty;
-                token = jsonObject[tokenClaim]?.GetValue<string>();
+                token = jsonObject[privateToken.TokenClaim]?.GetValue<string>();
             }
+            return token ?? string.Empty;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+    public async Task<string> GetAdminToken()
+    {
+        if (adminToken is null)
+            throw new ArgumentNullException("Admin token configuration not found");
+        try
+        {
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            if (adminToken.IgnoreSslVerification)
+            {
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            }
+            HttpClient httpClient = new HttpClient(httpClientHandler);
+            var parameters = new Dictionary<string, string>
+            {
+                {"client_id", adminToken.ClientId},
+                {"username", adminToken.Username},
+                {"grant_type", "password"},
+                {"password",adminToken.Password }
+            };
+            var content = new FormUrlEncodedContent(parameters);
+            var response = await httpClient.PostAsync(adminToken.AuthenticationUrl, content);
+            if (!response.IsSuccessStatusCode) return string.Empty;
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            string? token = responseString;
+            var jsonObject = JsonSerializer.Deserialize<JsonObject>(responseString);
+            if (jsonObject is null) return string.Empty;
+            token = jsonObject["access_token"]?.GetValue<string>();
             return token ?? string.Empty;
         }
         catch (Exception)
---
+++ src/Server/SharedApiUtils.Abstractons/AuthenticationTokenAccessor/IAuthenticationTokenAccessorBuilder.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SharedApiUtils.Abstractons.AuthenticationTokenAccessor;
+
+public interface IAuthenticationTokenAccessorBuilder
+{
+    IAuthenticationTokenAccessorBuilder SetPrivateTokenInfo(IConfigurationSection privateTokenSection);
+    IAuthenticationTokenAccessorBuilder SetAdminTokenInfo(IConfigurationSection adminTokenSection);
+    AuthenticationTokenAccessor Build();
+}
