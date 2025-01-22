02d4f293ab8836bc234732487d5f9f606d1f6402
Ivan Hunko <ivan.hunko.dan@gmail.com>
2025-01-22 19:41:50 +0200

    added get last season user score

--- src/Server/Services/RatingService/Services/RatingServiceController.cs
+++ src/Server/Services/RatingService/Services/RatingServiceController.cs
@@ -55,16 +55,14 @@ public class RatingServiceController : ControllerBase
     {
         string? userIdClaim = userContext.GetUserId(HttpContext);
         if(string.IsNullOrEmpty(userIdClaim))return BadRequest(ErrorMessages.PreferedUsernameClaimNotFound);
-
+        var userScore = await ratingService.GetUserScore(targetUserId, seasonId);
         if (User.IsInRole(authSettings.AdminRoleClaim))
         {
-            var userScore = await ratingService.GetUserScore(targetUserId, seasonId);
             if (userScore is null) return NotFound(ErrorMessages.NoUserScore);
             return Ok(userScore);
         }
-        if(userIdClaim == targetUserId)
+        if(userIdClaim.ToLower() == targetUserId.ToLower())
         {
-            var userScore = await ratingService.GetUserScore(targetUserId, seasonId);
             if (userScore is null) return NotFound(ErrorMessages.NoUserScore);
             return Ok(userScore);
         }
@@ -74,7 +72,6 @@ public class RatingServiceController : ControllerBase
 
         if (!isPrivateProfile.Value)
         {
-            var userScore = await ratingService.GetUserScore(targetUserId, seasonId);
             if (userScore is null) return NotFound(ErrorMessages.NoUserScore);
             return Ok(userScore);
         }
@@ -83,6 +80,41 @@ public class RatingServiceController : ControllerBase
             return Forbid();
         }
         
+    }
+
+    [Authorize(Policy = "AllAuthenticatedUsers")]
+    [HttpGet("GetScore/{targetUserId}")]
+    public async Task<IActionResult> GetLastSeasonUserScore([FromRoute] string targetUserId)
+    {
+        string? userIdClaim = userContext.GetUserId(HttpContext);
+        if (string.IsNullOrEmpty(userIdClaim)) return BadRequest(ErrorMessages.PreferedUsernameClaimNotFound);
+        var season = await ratingService.GetCurrentSeason();
+        if (season is null)
+            return StatusCode(500, ErrorMessages.NoCurrentSeason);
+        var userScore = await ratingService.GetUserScore(targetUserId, season.SeasonId);
+        if (User.IsInRole(authSettings.AdminRoleClaim))
+        {
+            if (userScore is null) return NotFound(ErrorMessages.NoUserScore);
+            return Ok(userScore);
+        }
+        if (userIdClaim.ToLower() == targetUserId.ToLower())
+        {
+            if (userScore is null) return NotFound(ErrorMessages.NoUserScore);
+            return Ok(userScore);
+        }
+        bool? isPrivateProfile = await profileService.UserProfileIsPrivate(targetUserId);
+        if (isPrivateProfile is null)
+            return StatusCode(500);
+
+        if (!isPrivateProfile.Value)
+        {
+            if (userScore is null) return NotFound(ErrorMessages.NoUserScore);
+            return Ok(userScore);
+        }
+        else
+        {
+            return Forbid();
+        }
     }
 
     [Authorize(Policy = "AdminOrPrivateClient")]
